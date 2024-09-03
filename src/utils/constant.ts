2ddcccafd55713f4c1a165154dc4610ef7c0c3ae
God Hades <155072724+GodHad@users.noreply.github.com>
2024-09-03 05:20:02 -0700

    Update constant.ts

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,1 +1,1 @@
-export const apiUrl: string = import.meta.env.VITE_API_BASE_URL || 'https://omegle-backend-beta.vercel.app/';
+export const apiUrl: string = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/';
