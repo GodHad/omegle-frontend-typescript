d432ead6179f98fd660f4c0f9a0a287ff630b847
God Hades <155072724+GodHad@users.noreply.github.com>
2024-09-03 05:33:21 -0700

    Update constant.ts

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,1 +1,1 @@
-export const apiUrl: string = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/';
+export const apiUrl: string = import.meta.env.VITE_API_BASE_URL || 'https://620d-95-217-43-132.ngrok-free.app';
